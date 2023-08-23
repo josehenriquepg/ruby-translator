c16d57483a2fae83acd8ed8ca3045a3cd5ce84b8
Henrique Gomes <henricopg11@gmail.com>
2023-08-23 00:59:50 -0300

    feat: first commit

---
+++ translator.rb
@@ -0,0 +1,46 @@
+require 'rest-client'
+require 'json'
+require 'fileutils'
+
+class Translator
+  def self.translate(text, source_lang, target_lang)
+    url = "https://api.mymemory.translated.net/get"
+    params = {
+      'q' => text,
+      'langpair' => "#{source_lang}|#{target_lang}" 
+    }
+
+    response = RestClient.get(url, params: params)
+    translation = JSON.parse(response.body)["responseData"]["translatedText"]
+    translation
+  end
+end
+
+puts "Bem-vindo ao RubyTradutor!"
+
+puts "Idioma original(en, pt, es, fr...): "
+source_lang = gets.chomp
+
+puts "Idioma para tradução(en, pt, es, fr...): "
+target_lang = gets.chomp
+
+puts "Digite aqui o texto que deseja traduzir: "
+original_text = gets.chomp
+
+translated_text = Translator.translate(original_text, source_lang, target_lang)
+
+puts "Tradução: #{translated_text}"
+
+base_path = "Ruby-Translate/translations"
+
+FileUtils.mkdir_p(base_path) unless File.directory?(base_path)
+
+timestamp = Time.now.strftime("%d-%m-%Y_%H-%M-%S")
+filename = "#{base_path}/#{timestamp}.txt"
+
+File.open(filename, 'w') do |file|
+  file.puts("Texto Original:\n  #{original_text}")
+  file.puts("\nTexto Traduzido:\n  #{translated_text}")
+end
+
+puts "Sua tradução foi salva em: #{filename}"
